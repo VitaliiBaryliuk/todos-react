bf4f08fd535c40a3e4ceed0c6785d822bd39e49d
VitaliiBaryliuk <vitaliyua912@gmail.com>
2019-02-28 02:25:38 +0200

    updated for gh-pages

---
+++ precache-manifest.9ad92ec13acce55b09a42a3f9d618421.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f008eeb3ae5bbc162a40",
+    "url": "/todos-react/static/js/runtime~main.f008eeb3.js"
+  },
+  {
+    "revision": "317bcd636ef9bdde4442",
+    "url": "/todos-react/static/js/main.317bcd63.chunk.js"
+  },
+  {
+    "revision": "5fea0a9e6b8b3a0fccd4",
+    "url": "/todos-react/static/js/2.5fea0a9e.chunk.js"
+  },
+  {
+    "revision": "317bcd636ef9bdde4442",
+    "url": "/todos-react/static/css/main.f7e12b5b.chunk.css"
+  },
+  {
+    "revision": "d3ccca59f4cea835cfb78661b9792363",
+    "url": "/todos-react/index.html"
+  }
+];
